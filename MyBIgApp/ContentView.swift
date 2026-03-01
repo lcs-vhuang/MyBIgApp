a8ac600fcf3a1dcbfc25ce0923f35b4e86eb21c3
Venessa Huang <vhuang@lcs.on.ca>
2026-03-01 13:10:09 -0500

    Initial Commit

---
+++ MyBIgApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBIgApp
+//
+//  Created by 黃翊喬 on 2026/3/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBIgApp/MyBIgAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBIgAppApp.swift
+//  MyBIgApp
+//
+//  Created by 黃翊喬 on 2026/3/1.
+//
+
+import SwiftUI
+
+@main
+struct MyBIgAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
